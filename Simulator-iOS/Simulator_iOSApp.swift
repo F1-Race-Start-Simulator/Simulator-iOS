990c62f1dd56e56da76ba560c982033b7ab75d12
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2024-10-16 15:52:43 +0200

    Add Xcode iOS basics

---
+++ Simulator-iOS/Simulator_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simulator_iOSApp.swift
+//  Simulator-iOS
+//
+//  Created by Florian DAVID on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Simulator_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
