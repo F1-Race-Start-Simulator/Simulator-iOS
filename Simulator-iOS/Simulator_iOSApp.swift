13eef4e220e51986297ede6aadbbedadf2d1cf57
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2024-10-27 19:21:11 +0100

    Add fonts

--- Simulator-iOS/Simulator_iOSApp.swift
+++ Simulator-iOS/Simulator_iOSApp.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 @main
 struct Simulator_iOSApp: App {
+    init() {
+        setFontsForBars()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    func setFontsForBars() {
+        let attributesBold: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "Formula1-Display-Bold", size: 18)!,
+            .textEffect: NSAttributedString.TextEffectStyle.letterpressStyle,
+        ]
+        
+        let attributesReg: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "Formula1-Display-Regular", size: 14)!,
+            .textEffect: NSAttributedString.TextEffectStyle.letterpressStyle
+        ]
+        
+        UIBarButtonItem.appearance().setTitleTextAttributes(attributesReg, for: .normal)
+        UINavigationBar.appearance().titleTextAttributes = attributesBold
     }
 }
